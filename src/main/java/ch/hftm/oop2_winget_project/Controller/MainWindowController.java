f47ef87c60287089bd3dc68b35edf3d3e9bff491
Andres Soria <andres.soria@hftm.ch>
2023-11-25 16:19:44 +0100

    changed mainwindow controller

--- src/main/java/ch/hftm/oop2_winget_project/Controller/MainWindowController.java
+++ src/main/java/ch/hftm/oop2_winget_project/Controller/MainWindowController.java
@@ -1,6 +1,23 @@
 package ch.hftm.oop2_winget_project.Controller;
 
-public class MainWindowController
-{
+import ch.hftm.oop2_winget_project.Features.WinGetQuery;
+import ch.hftm.oop2_winget_project.Utils.StageAndSceneManager;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindowController implements Initializable
+{
+    @FXML
+    private BorderPane mainWindowBorderPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        WinGetQuery.setWinGetLanguage();
+        StageAndSceneManager.loadFxmlToBorderPaneCenter(mainWindowBorderPane, "Views/SearchPackages");
+    }
 }

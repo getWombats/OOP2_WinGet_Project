26b777fce2b369a6c589eb58709d11dd3244fc29
Andres Soria <andres.soria@hftm.ch>
2023-11-25 16:17:50 +0100

    added interface for controller classes

---
+++ src/main/java/ch/hftm/oop2_winget_project/Api/TableViewModificationable.java
@@ -0,0 +1,14 @@
+package ch.hftm.oop2_winget_project.Api;
+
+import ch.hftm.oop2_winget_project.Models.WinGetPackage;
+
+public interface TableViewModificationable
+{
+    void setTableViewData();
+
+    void setTableViewSource();
+
+    void refreshTableViewContent();
+
+    WinGetPackage getObjectFromSelection();
+}

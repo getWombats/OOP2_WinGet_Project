c3c81dd92c57ea0b1d5cd66e38ccc62c159a6785
Andres Soria <andres.soria@hftm.ch>
2023-11-27 22:25:09 +0100

    refactored class

--- src/main/java/ch/hftm/oop2_winget_project/Utils/SystemLanguage.java
+++ src/main/java/ch/hftm/oop2_winget_project/Utils/SystemLanguage.java
@@ -6,11 +6,12 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SystemLanguage {
-
+public class SystemLanguage
+{
     private static long consoleExitCode;
 
-    public static String getPreferedLanguage() {
+    public static String getPreferedLanguage()
+    {
         try
         {
             List<String> rawDataList = new ArrayList<>();
@@ -21,17 +22,12 @@ public class SystemLanguage {
             processBuilderLanguage.redirectErrorStream(true);
             Process process_getPrefLang = processBuilderLanguage.start();
 
-            if(!process_getPrefLang.isAlive())
-            {
-                consoleExitCode = 66;
-            }
-
-
             BufferedReader reader = new BufferedReader(new InputStreamReader(process_getPrefLang.getInputStream()));
 
             String readerLine;
 
-            while ((readerLine = reader.readLine()) != null) {
+            while ((readerLine = reader.readLine()) != null)
+            {
                 rawDataList.add(readerLine);
             }
 
@@ -39,27 +35,31 @@ public class SystemLanguage {
 
             consoleExitCode = process_getPrefLang.waitFor();
 
-
             for(String line : rawDataList)
             {
-                if(isLanguageLine(line)){
+                if(line.contains("Languages"))
+                {
                     String preferedLanguages = line.substring(line.indexOf("REG_MULTI_SZ") + "REG_MULTI_SZ".length()).trim();
                     prefferedLanguage = preferedLanguages.substring(0, 2);
+                    break;
                 }
             }
             return prefferedLanguage;
         }
-        catch (IOException ex) {
+        catch (IOException ex)
+        {
             ex.printStackTrace();
-            return "en";
-        } catch (InterruptedException e) {
+        }
+        catch (InterruptedException e)
+        {
             throw new RuntimeException(e);
         }
+
+        return "";
     }
 
-
-
-    public static boolean isLanguageLine(String line){
-        return line.contains("Languages");
+    public static long getPromptExitCode()
+    {
+        return consoleExitCode;
     }
 }

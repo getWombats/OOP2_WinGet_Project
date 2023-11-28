49fc47b6707aedd5d4f8e3158c9d6efaff533db1
Andres Soria <andres.soria@hftm.ch>
2023-11-28 09:03:57 +0100

    added WinGetSettings object to set language

--- src/main/java/ch/hftm/oop2_winget_project/App.java
+++ src/main/java/ch/hftm/oop2_winget_project/App.java
@@ -1,34 +1,35 @@
 package ch.hftm.oop2_winget_project;
 
-import ch.hftm.oop2_winget_project.Features.WinGetQuery;
-import ch.hftm.oop2_winget_project.Utils.SystemLanguage;
+import ch.hftm.oop2_winget_project.Models.WinGetSettings;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Locale;
 
 public class App extends Application
 {
+    public static WinGetSettings winGetSettings;
+
     @Override
     public void start(Stage stage) throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Views/MainWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1180, 480);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("WinGet Project");
         stage.setScene(scene);
-        Locale defaultLocale = Locale.getDefault();
-        System.out.println(defaultLocale);
-        String displayCountry = defaultLocale.getDisplayCountry();
-        System.out.println(displayCountry);
-        String language = defaultLocale.getLanguage();
-        System.out.println(language);
-        String country = defaultLocale.getCountry();
-        System.out.println(country);
+//        Locale defaultLocale = Locale.getDefault();
+//        System.out.println(defaultLocale);
+//        String displayCountry = defaultLocale.getDisplayCountry();
+//        System.out.println(displayCountry);
+//        String language = defaultLocale.getLanguage();
+//        System.out.println(language);
+//        String country = defaultLocale.getCountry();
+//        System.out.println(country);
 
-        WinGetQuery.setWinGetLanguage(); // Get system language and set
+        winGetSettings = new WinGetSettings();
+        winGetSettings.setWinGetLanguage();
         stage.show();
     }
 

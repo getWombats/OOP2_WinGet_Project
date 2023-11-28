dfba9b56a853d785ad5987dc3f945505546943b2
Luca Bützberger <luca.buetzberger@gmail.com>
2023-11-28 18:50:51 +0100

    Working on Upgrade Packages

--- src/main/java/ch/hftm/oop2_winget_project/Controller/UpdatePackagesController.java
+++
@@ -1,6 +0,0 @@
-package ch.hftm.oop2_winget_project.Controller;
-
-public class UpdatePackagesController
-{
-    // Controller for updating installed packages view
-}
---
+++ src/main/java/ch/hftm/oop2_winget_project/Controller/UpgradePackagesController.java
@@ -0,0 +1,23 @@
+package ch.hftm.oop2_winget_project.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class UpgradePackagesController
+{
+
+    public TableView searchTableView;
+    public TableColumn nameColumn;
+    public TableColumn idColumn;
+    public TableColumn versionColumn;
+    public TableColumn sourceColumn;
+
+    @FXML
+    private void upgradeButtonClick()
+    {
+    }
+
+    // Controller for updating installed packages view
+}

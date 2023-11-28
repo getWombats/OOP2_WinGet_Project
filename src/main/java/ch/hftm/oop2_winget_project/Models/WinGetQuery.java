093a4859c29db262eb1094b7c09a5a77f44d3ee5
Andres Soria <andres.soria@hftm.ch>
2023-11-28 20:54:02 +0100

    removed try catch block

--- src/main/java/ch/hftm/oop2_winget_project/Models/WinGetQuery.java
+++ src/main/java/ch/hftm/oop2_winget_project/Models/WinGetQuery.java
@@ -25,107 +25,96 @@ public class WinGetQuery
     private long consoleExitCode;
     private final Pattern VALIDLINE_REGEX = Pattern.compile("[-▒█\\\\|]");
 
-    public void QueryToList(QueryType queryType, String keyWord, ObservableList<WinGetPackage> packageList)
+    public void queryToList(QueryType queryType, String keyWord, ObservableList<WinGetPackage> packageList) throws IOException, InterruptedException
     {
-        try
+        List<String> rawDataList = new ArrayList<>();
+
+        ProcessBuilder processBuilder = new ProcessBuilder("winget", queryType.toString(), keyWord);
+        processBuilder.redirectErrorStream(true);
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+        String readerLine;
+
+        headerCounter = 0;
+        int columnSeparatorIndexId = -1;
+        int columnSeparatorIndexVersion = -1;
+        int columnSeparatorIndexAvailableOrMatch = -1;
+        int columnSeparatorIndexSource = -1;
+        int maxPackageLineLength = -1;
+
+        while ((readerLine = reader.readLine()) != null)
         {
-            List<String> rawDataList = new ArrayList<>();
+            rawDataList.add(readerLine);
+        }
+        reader.close();
 
-            ProcessBuilder processBuilder = new ProcessBuilder("winget", queryType.toString(), keyWord);
-            processBuilder.redirectErrorStream(true);
-            Process process = processBuilder.start();
+        consoleExitCode = process.waitFor();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String readerLine;
-
-            headerCounter = 0;
-            int columnSeparatorIndexId = -1;
-            int columnSeparatorIndexVersion = -1;
-            int columnSeparatorIndexAvailableOrMatch = -1;
-            int columnSeparatorIndexSource = -1;
-            int maxPackageLineLength = -1;
-
-            while ((readerLine = reader.readLine()) != null)
+        if(consoleExitCode != ConsoleExitCode.NO_PACKAGE_FOUND.getValue())
+        {
+            for(String line : rawDataList)
             {
-                rawDataList.add(readerLine);
-            }
-            reader.close();
-
-            consoleExitCode = process.waitFor();
-
-            if(consoleExitCode != ConsoleExitCode.NO_PACKAGE_FOUND.getValue())
-            {
-                for(String line : rawDataList)
+                // Get index when each column begins as separator if line is table header, only executes once
+                if(isHeaderLine(line))
                 {
-                    // Get index when each column begins as separator if line is table header, only executes once
-                    if(isHeaderLine(line))
+                    columnSeparatorIndexId = line.toLowerCase().indexOf(columnHeaderIdText);
+                    columnSeparatorIndexVersion = line.toLowerCase().indexOf(columnHeaderVersionText);
+
+                    if(line.toLowerCase().contains(columnHeaderMatchText))
                     {
-                        columnSeparatorIndexId = line.toLowerCase().indexOf(columnHeaderIdText);
-                        columnSeparatorIndexVersion = line.toLowerCase().indexOf(columnHeaderVersionText);
-
-                        if(line.toLowerCase().contains(columnHeaderMatchText))
-                        {
-                            columnSeparatorIndexAvailableOrMatch = line.toLowerCase().indexOf(columnHeaderMatchText);
-                        }
-                        else if (line.toLowerCase().contains(columnHeaderAvailableText))
-                        {
-                            columnSeparatorIndexAvailableOrMatch = line.toLowerCase().indexOf(columnHeaderAvailableText);
-                        }
-
-                        columnSeparatorIndexSource = line.toLowerCase().indexOf(columnHeaderSourceText);
-                        maxPackageLineLength = columnSeparatorIndexSource + SourceType.MSSTORE.toString().length();
-
-                        headerCounter++;
+                        columnSeparatorIndexAvailableOrMatch = line.toLowerCase().indexOf(columnHeaderMatchText);
+                    }
+                    else if (line.toLowerCase().contains(columnHeaderAvailableText))
+                    {
+                        columnSeparatorIndexAvailableOrMatch = line.toLowerCase().indexOf(columnHeaderAvailableText);
                     }
 
-                    // Get actual package information
-                    if(isPackageLine(line))
+                    columnSeparatorIndexSource = line.toLowerCase().indexOf(columnHeaderSourceText);
+                    maxPackageLineLength = columnSeparatorIndexSource + SourceType.MSSTORE.toString().length();
+
+                    headerCounter++;
+                }
+
+                // Get actual package information
+                if(isPackageLine(line))
+                {
+                    // Fix some weird asia packages
+                    if(containsHanScript(line))
                     {
-                        // Fix some weird asia packages
-                        if(containsHanScript(line))
-                        {
-                            int missingLength = maxPackageLineLength - line.length() - 1;
+                        int missingLength = maxPackageLineLength - line.length() - 1;
 
-                            StringBuilder uglyPatch = new StringBuilder();
-                            uglyPatch.append(" ".repeat(Math.max(0, missingLength)));
+                        StringBuilder uglyPatch = new StringBuilder();
+                        uglyPatch.append(" ".repeat(Math.max(0, missingLength)));
 
-                            line = line.substring(0, columnSeparatorIndexId-missingLength) + uglyPatch + line.substring(columnSeparatorIndexId-missingLength);
-                        }
+                        line = line.substring(0, columnSeparatorIndexId-missingLength) + uglyPatch + line.substring(columnSeparatorIndexId-missingLength);
+                    }
 
-                        String packageVersion;
-                        if(columnSeparatorIndexAvailableOrMatch > -1)
-                        {
-                            packageVersion = line.substring(columnSeparatorIndexVersion, columnSeparatorIndexAvailableOrMatch).trim();
-                        }
-                        else
-                        {
-                            packageVersion = line.substring(columnSeparatorIndexVersion, columnSeparatorIndexSource).trim();
-                        }
+                    String packageVersion;
+                    if(columnSeparatorIndexAvailableOrMatch > -1)
+                    {
+                        packageVersion = line.substring(columnSeparatorIndexVersion, columnSeparatorIndexAvailableOrMatch).trim();
+                    }
+                    else
+                    {
+                        packageVersion = line.substring(columnSeparatorIndexVersion, columnSeparatorIndexSource).trim();
+                    }
 
-                        WinGetPackage winGetPackage = new WinGetPackage(
-                                line.substring(0, columnSeparatorIndexId).trim(), // Package Name
-                                line.substring(columnSeparatorIndexId, columnSeparatorIndexVersion).trim(), // Package ID
-                                packageVersion,
-                                line.substring(columnSeparatorIndexSource).trim() // Package Source
-                        );
+                    WinGetPackage winGetPackage = new WinGetPackage(
+                            line.substring(0, columnSeparatorIndexId).trim(), // Package Name
+                            line.substring(columnSeparatorIndexId, columnSeparatorIndexVersion).trim(), // Package ID
+                            packageVersion,
+                            line.substring(columnSeparatorIndexSource).trim() // Package Source
+                    );
 
-                        // Lock the list object
-                        synchronized(packageList)
-                        {
-                            packageList.add(winGetPackage);
-                        }
+                    // Lock the list object
+                    synchronized(packageList)
+                    {
+                        packageList.add(winGetPackage);
                     }
                 }
             }
-        }
-        catch (IOException | InterruptedException ex)
-        {
-            // Exceptionhandler
-        }
-        catch (IndexOutOfBoundsException ex)
-        {
-            ex.printStackTrace();
         }
     }
 

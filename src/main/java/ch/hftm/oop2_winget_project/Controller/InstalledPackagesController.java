3a2fa5b015b05002f9f25e67ed97281b7590fbf2
Andres Soria <andres.soria@hftm.ch>
2023-11-29 22:24:13 +0100

    added new fxml and controller

--- src/main/java/ch/hftm/oop2_winget_project/Controller/InstalledPackagesController.java
+++ src/main/java/ch/hftm/oop2_winget_project/Controller/InstalledPackagesController.java
@@ -1,21 +1,29 @@
 package ch.hftm.oop2_winget_project.Controller;
 
 import ch.hftm.oop2_winget_project.Api.TableViewModificationable;
+import ch.hftm.oop2_winget_project.App;
+import ch.hftm.oop2_winget_project.Features.QueryType;
 import ch.hftm.oop2_winget_project.Models.WinGetPackage;
+import ch.hftm.oop2_winget_project.Models.WinGetQuery;
+import ch.hftm.oop2_winget_project.Utils.ConsoleExitCode;
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.text.Text;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class InstalledPackagesController implements TableViewModificationable, Initializable
 {
-    // Controller for installed packages view
     @FXML
     private TableView<WinGetPackage> installedPackagesTableView;
     @FXML
@@ -29,41 +37,92 @@ public class InstalledPackagesController implements TableViewModificationable, I
     @FXML
     private Label tableViewPlaceholderLabel;
     private boolean isThreadWorking;
-    private static final ObservableList<WinGetPackage> packageList = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
+        tableViewPlaceholderLabel = new Label();
 
+        addButtonToTableView();
+        setTableViewData();
+        setTableViewSource();
+
+        tableViewPlaceholderLabel.setText("Liste installierter Pakete");
+        installedPackagesTableView.setPlaceholder(tableViewPlaceholderLabel);
+    }
+
+    @FXML
+    private void testButtonClick()
+    {
+        showInstalledPackages();
     }
 
     @Override
     public void setTableViewData()
     {
-
+        this.nameColumn.setCellValueFactory(cellData -> cellData.getValue().packageNameProperty());
+        this.idColumn.setCellValueFactory(cellData -> cellData.getValue().packageIDProperty());
+        this.versionColumn.setCellValueFactory(cellData -> cellData.getValue().packageVersionProperty());
+        this.sourceColumn.setCellValueFactory(cellData -> cellData.getValue().packageSourceProperty());
     }
 
     @Override
     public void setTableViewSource()
     {
-
+        this.installedPackagesTableView.setItems(App.getListManager().getInstalledPackageList());
     }
 
     @Override
     public void refreshTableViewContent()
     {
-
+        installedPackagesTableView.setItems(null);
+        setTableViewSource();
     }
 
     @Override
     public WinGetPackage getObjectFromSelection()
     {
-        return null;
+        return installedPackagesTableView.getSelectionModel().getSelectedItem();
     }
 
     @Override
     public void addButtonToTableView()
     {
 
+    }
+
+    private void showInstalledPackages()
+    {
+        if (!isThreadWorking)
+        {
+            installedPackagesTableView.getItems().clear();
+
+            tableViewPlaceholderLabel.setText("Installierte Pakete werden geladen...");
+
+            isThreadWorking = true;
+            new Thread(() -> {
+                WinGetQuery query = new WinGetQuery();
+                try
+                {
+                    query.queryToList(QueryType.LIST, "", App.getListManager().getInstalledPackageList());
+                }
+                catch (IOException | InterruptedException ex)
+                {
+                    System.out.println(ex.getMessage());
+                }
+
+                Platform.runLater(() -> {
+                    if (query.getConsoleExitCode() == ConsoleExitCode.OK.getValue())
+                    {
+                        refreshTableViewContent();
+                    }
+                    else
+                    {
+                        tableViewPlaceholderLabel.setText("Error oder so?");
+                    }
+                    isThreadWorking = false;
+                });
+            }).start();
+        }
     }
 }

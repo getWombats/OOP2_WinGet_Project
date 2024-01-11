fce29551f5185bc5a967b46ed9ac9176d4ccd444
Andres Soria <andres.soria@hftm.ch>
2024-01-11 20:14:03 +0100

    added new input validation class

---
+++ src/main/java/ch/hftm/oop2_winget_project/Service/InputValidator.java
@@ -0,0 +1,22 @@
+package ch.hftm.oop2_winget_project.Service;
+
+import javafx.scene.control.TextFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputValidator
+{
+    private static final Pattern INVALID_INPUT_REGEX = Pattern.compile("^[a-zA-Z0-9]+$");
+
+    public static TextFormatter<String> createSpaceValidator()
+    {
+        return new TextFormatter<>(change -> {
+            Matcher matcher = INVALID_INPUT_REGEX.matcher(change.getText());
+            if (!matcher.find())
+            {
+                change.setText("");
+            }
+            return change;
+        });
+    }
+}

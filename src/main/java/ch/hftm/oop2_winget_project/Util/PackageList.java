07dcedd8ecab25a463ad119b1ee92af6845c7927
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-02-08 15:03:23 +0100

    Added getName method to packageList

--- src/main/java/ch/hftm/oop2_winget_project/Util/PackageList.java
+++ src/main/java/ch/hftm/oop2_winget_project/Util/PackageList.java
@@ -1,14 +1,23 @@
 package ch.hftm.oop2_winget_project.Util;
 
 import ch.hftm.oop2_winget_project.Model.WinGetPackage;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class PackageList {
 
-
+// Variables
     private String packageListName;
-    private int packageListSize;
-    private List<WinGetPackage> packages;
+    private ArrayList<WinGetPackage> packages;
 
+//    Constructors
+    public PackageList(String packageListName){
+        this.packageListName = packageListName;
+    }
+
+
+//    Methods
+    public String getName(){
+        return packageListName;
+    }
 }
